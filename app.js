4684c95cab308eac643a3e3bf429be1c098a598d
Bello Roqeeb <roqbell@gmail.com>
2022-09-13 09:17:48 +0100

    registration form

---
+++ app.js
@@ -0,0 +1,15 @@
+let x = document.querySelector("#login");
+let y = document.querySelector("#register");
+let z = document.querySelector("#btn");
+
+function login() {
+    x.style.left = "-400px"; 
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+function register() {
+    x.style.left = "50px"; 
+    y.style.left = "450px";
+    z.style.left = "0";
+}
